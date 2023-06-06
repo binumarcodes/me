933bf830be6aa0d59e2660e0f85f45e1e984a1a5
Ibrahim <i.aliyu019@gmail.com>
2023-06-06 13:31:44 +0100

    modified  navigations

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,122 +1,163 @@
-import { Button, Container, IconButton, makeStyles, Typography } from '@material-ui/core'
-import { SendOutlined, WhatsApp, FileCopy, ArrowUpward, ArrowUpwardSharp, ArrowUpwardOutlined } from '@material-ui/icons'
-import React from 'react'
-import ReactTyped from 'react-typed'
+import {
+  Button,
+  Container,
+  IconButton,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import {
+  SendOutlined,
+  WhatsApp,
+  FileCopy,
+  ArrowUpward,
+  ArrowUpwardSharp,
+  ArrowUpwardOutlined,
+} from "@material-ui/icons";
+import React from "react";
+import ReactTyped from "react-typed";
 
-const useStyles = makeStyles((theme)=>({
-    section:{
-        backgroundColor: theme.palette.secondary.main,
-        [theme.breakpoints.down('sm')]:{
-            paddingTop: '5rem '
-        }
-
+const useStyles = makeStyles((theme) => ({
+  section: {
+    backgroundColor: theme.palette.secondary.main,
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: "5rem ",
     },
-    h6:{
-        backgroundColor: "rgba(123, 74, 226, 0.05)",
-        padding: '.5rem 1rem',
-        color: theme.palette.secondary.light,
-        borderRadius: '1.6rem',
-        width: 'fit-content',
-        margin: 'auto'
+  },
+  h6: {
+    backgroundColor: "rgba(123, 74, 226, 0.05)",
+    padding: ".5rem 1rem",
+    color: theme.palette.secondary.light,
+    borderRadius: "1.6rem",
+    width: "fit-content",
+    margin: "auto",
+  },
+  h1: {
+    color: theme.palette.primary.main,
+    fontWeight: 700,
+    maxWidth: "100%",
+    lineHeight: "7rem",
+    marginRight: "auto",
+    textAlign: "center",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "3rem",
     },
-    h1:{
-        color: theme.palette.primary.main,
-        fontWeight: 700,
-        maxWidth: '100%',
-        lineHeight: '7rem',
-        marginRight: 'auto',
-        textAlign: 'center',
-        [theme.breakpoints.down('sm')]:{
-            fontSize: '3rem',
-        }
-
+  },
+  whatsAppBtn: {
+    display: "flex",
+    borderColor: theme.palette.secondary.light,
+    color: theme.palette.secondary.light,
+    fontWeight: 500,
+    width: "fit-content",
+    [theme.breakpoints.down("sm")]: {
+      margin: "auto",
     },
-    whatsAppBtn:{
-        display:'flex',
-        borderColor: theme.palette.secondary.light,
-        color: theme.palette.secondary.light,
-        fontWeight: 500,
-        width: 'fit-content',
-        [theme.breakpoints.down('sm')]:{
-            margin: 'auto'
-        }
-
-    },  
-    email:{
-        color: theme.palette.secondary.light,
-        cursor: "pointer"
+  },
+  email: {
+    color: theme.palette.secondary.light,
+    cursor: "pointer",
+  },
+  Contact: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "10rem",
+    marginTop: "5rem",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      gap: "2rem",
+      marginTop: "0rem",
     },
-    Contact:{
-        display: 'flex',
-        justifyContent: 'center',
-        gap: '10rem',
-        marginTop: '5rem',
-        [theme.breakpoints.down('sm')]:{
-            flexDirection: 'column',
-            gap: '2rem',
-            marginTop: '0rem'
-        }
-
+  },
+  icon: {
+    color: theme.palette.secondary.light,
+  },
+  p: {
+    fontSize: "1.5rem",
+    fontWeight: 100,
+    cursor: "pointer",
+  },
+  ToTop: {
+    marginTop: "10rem",
+    fontSize: "1rem",
+    fontWeight: "400",
+    color: theme.palette.secondary.light,
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "3rem",
     },
-    icon:{
-        color: theme.palette.secondary.light
-    },
-    p:{
-        fontSize:'1.5rem',
-        fontWeight: 100,
-        cursor: 'pointer'
-    },
-    ToTop:{
-        marginTop: '10rem',
-        fontSize: '1rem',
-        fontWeight: '400',
-        color: theme.palette.secondary.light, 
-        [theme.breakpoints.down('sm')]:{
-            marginTop: '3rem'
-        }
-
-    },
-    link:{
-        textDecoration: 'none',
-        fontWeight: 700,
-
-    }
-
-
-}))
+  },
+  link: {
+    textDecoration: "none",
+    fontWeight: 700,
+  },
+}));
 function Contact() {
-const copyText = () =>{
-    let toBeCopied = document.querySelector('#email').innerHTML;
+  const copyText = () => {
+    let toBeCopied = document.querySelector("#email").innerHTML;
     let popup = document.getElementById("show");
-    (navigator.clipboard.writeText(toBeCopied));
-    popup.style.display = 'block'
-    setTimeout(()=>{
-        popup.style.display = 'none'
-    }, 1000)
-}
-const classes = useStyles()
+    navigator.clipboard.writeText(toBeCopied);
+    popup.style.display = "block";
+    setTimeout(() => {
+      popup.style.display = "none";
+    }, 1000);
+  };
+  const classes = useStyles();
   return (
-    <section className={classes.section}>
-        <Container className = {classes.container}>
-            <Typography variant='h6' className={classes.h6}>Contact Me</Typography>
-            <Typography variant='h1'className={classes.h1}>Let's {<ReactTyped strings={['Chat!', 'Talk!', 'Build!']} typeSpeed = {100} backSpeed = {100} loop/>}</Typography>
-            <div className={classes.Contact}>
-                <Button variant='outlined'className={classes.whatsAppBtn} startIcon = {<WhatsApp/>} style = {{borderRadius:'1rem'}}> <a href = 'https://wa.me/2348163089308' target= '_blank' className={classes.link}>LETS HAVE A CONVERSATION</a></Button>
-                <div className={classes.email}  onClick = {copyText}>
-                    <Typography variant='body1'className={classes.p} id = 'show' style={{display: 'none', transition: 'all ease-in-out .2s'}}>copied!</Typography>
-                    <SendOutlined className={classes.icon}/>
-                    <Typography variant='body1'className={classes.p} id = 'email' >i.aliyu019@gmail.com</Typography>
-                    <FileCopy/>
-                </div>
+    <section className={classes.section} id="contact">
+      <Container className={classes.container}>
+        <Typography variant="h6" className={classes.h6}>
+          Contact Me
+        </Typography>
+        <Typography variant="h1" className={classes.h1}>
+          Let's{" "}
+          {
+            <ReactTyped
+              strings={["Chat!", "Talk!", "Build!"]}
+              typeSpeed={100}
+              backSpeed={100}
+              loop
+            />
+          }
+        </Typography>
+        <div className={classes.Contact}>
+          <Button
+            variant="outlined"
+            className={classes.whatsAppBtn}
+            startIcon={<WhatsApp />}
+            style={{ borderRadius: "1rem" }}
+          >
+            {" "}
+            <a
+              href="https://wa.me/2348163089308"
+              target="_blank"
+              className={classes.link}
+            >
+              LETS HAVE A CONVERSATION
+            </a>
+          </Button>
+          <div className={classes.email} onClick={copyText}>
+            <Typography
+              variant="body1"
+              className={classes.p}
+              id="show"
+              style={{ display: "none", transition: "all ease-in-out .2s" }}
+            >
+              copied!
+            </Typography>
+            <SendOutlined className={classes.icon} />
+            <Typography variant="body1" className={classes.p} id="email">
+              i.aliyu019@gmail.com
+            </Typography>
+            <FileCopy />
+          </div>
+        </div>
 
-            </div>
-
-                  <a href='#HeroSection' className={classes.link}><Button endIcon = {<ArrowUpwardOutlined/>}  className = {classes.ToTop}> Go to the top</Button></a>
-
-        
-        </Container>
-
+        <a href="#HeroSection" className={classes.link}>
+          <Button endIcon={<ArrowUpwardOutlined />} className={classes.ToTop}>
+            {" "}
+            Go to the top
+          </Button>
+        </a>
+      </Container>
     </section>
-  )
+  );
 }
-export default Contact
+export default Contact;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,91 +1,106 @@
-import { AppBar, Container, List, ListItem, makeStyles, Toolbar, Typography } from '@material-ui/core'
-import { hover } from '@testing-library/user-event/dist/hover'
+import {
+  AppBar,
+  Container,
+  List,
+  ListItem,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { hover } from "@testing-library/user-event/dist/hover";
 // import Logo from '../../publicImages/Logo.svg'
-import React from 'react'
+import React from "react";
 
-const useStyles = makeStyles((theme)=>({
-    AppBar:{
-        backgroundColor:'transparent',
-        marginTop: '3rem',
-        [theme.breakpoints.down('sm')]: {
-            margin: '0'
+const useStyles = makeStyles((theme) => ({
+  AppBar: {
+    backgroundColor: "transparent",
+    marginTop: "3rem",
+    [theme.breakpoints.down("sm")]: {
+      margin: "0",
     },
-
+  },
+  navbar: {
+    backgroundColor: "rgba(123, 74, 226, 0.05)",
+    borderRadius: "1.8rem",
+    display: "flex",
+    justifyContent: "space-between",
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
     },
-    navbar:{
-        backgroundColor: "rgba(123, 74, 226, 0.05)",
-        borderRadius: '1.8rem',
-        display:"flex",
-        justifyContent:'space-between',
-        [theme.breakpoints.down('xs')]: {
-            display: 'none'
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
-        [theme.breakpoints.down('sm')]: {
-            display: 'none'
+  },
+
+  menu: {
+    display: "flex",
+    color: theme.palette.primary.main,
+    fontSize: "1.5rem",
+    fontWeight: 100,
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    fontSize: "1.5rem",
+    fontWeight: 100,
+    color: theme.palette.primary.main,
+    "&:hover": {
+      color: theme.palette.secondary.light,
+      fontWeight: 700,
+      transition: "all ease-in-out .5s",
     },
-
+  },
+  menuItem: {
+    width: "fit-content",
+    color: "white",
+    "&:hover": {
+      color: theme.palette.secondary.light,
+      fontWeight: 700,
+      transition: "all ease-in-out .5s",
     },
-   
-    menu:{
-        display:'flex',
-        color: theme.palette.primary.main,
-        fontSize:'1.5rem',
-        fontWeight: 100,
-        cursor: 'pointer'
-    },
-    link:{
-        textDecoration: 'none', 
-        fontSize:'1.5rem',
-        fontWeight: 100,
-        color: theme.palette.primary.main,
-        '&:hover':{
-            color: theme.palette.secondary.light,
-            fontWeight: 700, 
-            transition: 'all ease-in-out .5s'
-        }
-
-
-    },
-    menuItem:{
-        width:'fit-content',
-        color: 'white',
-        '&:hover':{
-            color: theme.palette.secondary.light,
-            fontWeight: 700, 
-            transition: 'all ease-in-out .5s'
-        }
-    }
-
-
-    
-
-    
-
-})) 
+  },
+}));
 
 function Navbar() {
-const classes = useStyles()
+  const classes = useStyles();
   return (
-        <AppBar className={classes.AppBar} elevation = {0}>
-            <Container>
-            <Toolbar className={classes.navbar}>
-                <div className={classes.logo}>
-                    <img src='Images/Logo.svg' alt="" srcset="" />
-                </div>
-                <List className={classes.menu}>
-                    <ListItem className={classes.menuItem}><a href="#HeroSection" className={classes.link}>Home</a></ListItem>
-                    <ListItem className={classes.menuItem}><a href = '#AboutSection' className={classes.link}>About Me</a></ListItem>
-                    <ListItem className={classes.menuItem}>Projects</ListItem>
-                    <ListItem className={classes.menuItem}>Skills</ListItem>
-                    <ListItem className={classes.menuItem}>Testimonials</ListItem>
-                    <ListItem className={classes.menuItem}>Contact me</ListItem>
-                </List>
-
-            </Toolbar>
-            </Container>
-
-        </AppBar>
-  )
+    <AppBar className={classes.AppBar} elevation={0}>
+      <Container>
+        <Toolbar className={classes.navbar}>
+          <div className={classes.logo}>
+            <img src="Images/Logo.svg" alt="" srcset="" />
+          </div>
+          <List className={classes.menu}>
+            <ListItem className={classes.menuItem}>
+              <a href="#HeroSection" className={classes.link}>
+                Home
+              </a>
+            </ListItem>
+            <ListItem className={classes.menuItem}>
+              <a href="#AboutSection" className={classes.link}>
+                About Me
+              </a>
+            </ListItem>
+            <ListItem className={classes.menuItem}>
+              <a href="#projects" className={classes.link}>
+                Projects
+              </a>
+            </ListItem>
+            <ListItem className={classes.menuItem}>
+              <a href="#technology" className={classes.link}>
+                Technologies
+              </a>
+            </ListItem>
+            <ListItem className={classes.menuItem}>
+              <a href="#contact" className={classes.link}>
+                Contact Me
+              </a>
+            </ListItem>
+          </List>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
