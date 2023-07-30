c328887ece0d34d0273902d7b4242bef2da3bca9
Ibrahim <i.aliyu019@gmail.com>
2023-07-30 14:00:53 +0100

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
-import './App.css';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
-import HeroSection from './Components/HeroSection';
-import Navbar from './Components/Navbar';
-import AboutMe from './Components/AboutMe';
-import Project from './Components/Project';
-import Technology from './Components/Technology';
-import Career from './Components/Career';
-import Contact from './Components/Contact';
-
+import "./App.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import HeroSection from "./Components/HeroSection";
+import Navbar from "./Components/Navbar";
+import AboutMe from "./Components/AboutMe";
+import Project from "./Components/Project";
+import Technology from "./Components/Technology";
+import Career from "./Components/Career";
+import Contact from "./Components/Contact";
+import ReactGA from "react-ga";
 
 const theme = createMuiTheme({
-  palette:{
-    primary:{
-      main: "#FFFFFF"
+  palette: {
+    primary: {
+      main: "#FFFFFF",
     },
-    secondary:{
-      main:'#090E16',
-      light:'#7B4AE2',
-      dark: "#090E16"
+    secondary: {
+      main: "#090E16",
+      light: "#7B4AE2",
+      dark: "#090E16",
     },
-    success:{
-      main:'#4AE290'
-    }
+    success: {
+      main: "#4AE290",
+    },
   },
-  typography:{
-    fontFamily:'Raleway',
-
-  }
-})
+  typography: {
+    fontFamily: "Raleway",
+  },
+});
+const TRACKING_ID = "G-V730R3HMHD";
+ReactGA.initialize(TRACKING_ID);
+useEffect(() => {
+  ReactGA.pageview(window.location.pathname);
+}, []);
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Navbar/>
-        <HeroSection/>
-        <AboutMe/>
-        <Project/>
-        <Technology/>
-        <Career/>
-        <Contact/>
+        <Navbar />
+        <HeroSection />
+        <AboutMe />
+        <Project />
+        <Technology />
+        <Career />
+        <Contact />
       </div>
-
     </ThemeProvider>
   );
 }
-
+z;
 export default App;
